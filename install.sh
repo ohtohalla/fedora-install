71f79941dff8d284b9f68a2b1dbb309aeec0e6d8
Juho Kajava <44448646+ohtohalla@users.noreply.github.com>
2022-09-16 16:16:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "Fetching dotfiles"
 alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 echo ".dots" >> .gitignore
-git clone --bare <git-repo-url> $HOME/.dots
+git clone --bare git@github.com:ohtohalla/linux_dots.git $HOME/.dots
 alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 config checkout
 
